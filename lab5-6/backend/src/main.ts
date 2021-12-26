f4fb877a0d144276a0ba6391f59a65bbc2478e78
Ilya Nidzelsky <inidzelsky@gmail.com>
2021-12-26 23:16:25 +0200

    setup CORS and body parser

--- lab5-6/backend/src/main.ts
+++ lab5-6/backend/src/main.ts
@@ -1,10 +1,16 @@
 import { ValidationPipe } from "@nestjs/common"
 import { NestFactory } from "@nestjs/core"
 import { AppModule } from "./app.module"
+import * as bodyParser from "express"
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
 
+    app.use(bodyParser.json({ limit: "50mb" }))
+    app.enableCors({
+        origin: true,
+    })
+    
     app.useGlobalPipes(new ValidationPipe())
 
     await app.listen(3000)
