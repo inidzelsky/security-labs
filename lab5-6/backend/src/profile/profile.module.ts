cad3c5c95b4c4ddc8f2651b170d6621793f89ca6
Ilya Nidzelsky <inidzelsky@gmail.com>
2021-12-27 13:02:12 +0200

    implement Profile service methods

--- lab5-6/backend/src/profile/profile.module.ts
+++ lab5-6/backend/src/profile/profile.module.ts
@@ -1,9 +1,19 @@
 import { Module } from "@nestjs/common"
+import { DbModule } from "src/db/db.module"
+import { EncryptionModule } from "src/encryption/encryption.module"
 import { ProfileController } from "./profile.controller"
 import { ProfileService } from "./profile.service"
+import { UserPhoneRepository } from "./user-phone.repository"
 
 @Module({
+    imports: [
+        EncryptionModule,
+        DbModule,
+    ],
     controllers: [ProfileController],
-    providers: [ProfileService],
+    providers: [
+        ProfileService,
+        UserPhoneRepository,
+    ],
 })
 export class ProfileModule {}
--- lab5-6/backend/src/profile/profile.service.ts
+++ lab5-6/backend/src/profile/profile.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from "@nestjs/common"
+import { EncryptionService } from "src/encryption/encryption.service"
+import { UserPhone } from "./types/user-phone.type"
+import { UserPhoneRepository } from "./user-phone.repository"
 
 @Injectable()
-export class ProfileService {}
+export class ProfileService {
+    constructor(
+        private readonly encryptionService: EncryptionService,
+        private readonly userPhoneRepository: UserPhoneRepository,
+    ) {}
+    
+    public async savePhone({ userId, phone }: UserPhone): Promise<void> {
+        const encryptedPhone = await this.encryptionService.encrypt(
+            userId,
+            Buffer.from(phone)
+        )
+
+        if (await this.userPhoneRepository.findByUserId(userId))
+            throw new Error("user_phone_already_exists")
+
+        await this.userPhoneRepository.store({ userId, phone: encryptedPhone.toString() })
+    }
+
+    public async getPhone(userId: string): Promise<UserPhone> {
+        const userPhone = await this.userPhoneRepository.findByUserId(userId)
+        if (!userPhone)
+            throw new Error("user_phone_not_found")
+
+        const { phone } = userPhone
+        const decryptedPhone = await this.encryptionService.decrypt(userId, Buffer.from(phone))
+
+        return { userId, phone: decryptedPhone.toString() }
+    }
+}
