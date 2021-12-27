c677b54e6487f14bdaa8822524d02ffa80212793
Ilya Nidzelsky <inidzelsky@gmail.com>
2021-12-27 13:00:51 +0200

    add user_phones table schema

--- lab5-6/backend/src/db/schema.sql
+++ lab5-6/backend/src/db/schema.sql
@@ -1,6 +1,13 @@
 create table users (
-    id uuid default gen_random_uuid(),
+    id uuid default gen_random_uuid() not null,
     email varchar(255),
-    password varchar(255)
+    password varchar(255),
+    constraint pk_user primary key(id),
 );
 
+create table user_phones (
+    user_id uuid not null,
+    phone varchar(255),
+    constraint pk_user_phone primary key(user_id),
+    constraint fk_user_phone_user foreign key (user_id) references users(id)
+)
